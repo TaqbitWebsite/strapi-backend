2eb75528131af59b65388b7f7cd6feea7ff1eca4
Shaurya Nandwani <shaurya@taqbit.com>
2025-07-18 16:31:41 +0530

    Update server.ts

--- config/server.ts
+++ config/server.ts
@@ -2,8 +2,8 @@ export default ({ env }) => ({
   url: env('RENDER_EXTERNAL_URL'), // <- Important for Render deployment
 
   host: env('HOST', '0.0.0.0'),     // <- Keep this as is
-  port: env.int('PORT', 1337),       // <- Render will pass the correct PORT
-
+  port: env.int('PORT', 1337), // <- Render will pass the correct PORT
+  proxy: true,
   app: {
     keys: env.array('APP_KEYS'),    // <- Use environment variable for APP_KEYS (required)
   },
