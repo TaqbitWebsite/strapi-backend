f4595c116fffc19b7673905a9fedc4a256e2cae1
Shaurya Nandwani <shaurya@taqbit.com>
2025-07-18 16:05:23 +0530

    Update server.ts

--- config/server.ts
+++ config/server.ts
@@ -1,8 +1,15 @@
 export default ({ env }) => ({
-  url: env('RENDER_EXTERNAL_URL'),
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  url: env('RENDER_EXTERNAL_URL'), // <- Important for Render deployment
+
+  host: env('HOST', '0.0.0.0'),     // <- Keep this as is
+  port: env.int('PORT', 1337),       // <- Render will pass the correct PORT
+
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS'),    // <- Use environment variable for APP_KEYS (required)
+  },
+
+  admin: {
+    serveAdminPanel: true,          // <- Important! Ensures Strapi serves the admin panel in production
   },
 });
+
